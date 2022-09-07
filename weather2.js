0500f8682f5cd3c2ea1d356d4b79a9452bc6e517
Stephanie Kariuki <110028520+stephkariuki19@users.noreply.github.com>
2022-09-08 00:04:19 +0300

    Add files via upload

---
+++ weather2.js
@@ -0,0 +1,42 @@
+let weather = {
+  apiKey: "683ffa82b75c04c18c08847550a711cf",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        ",&units=metric&APPID=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+".png"
+    document.querySelector(".description").innerText  = description;
+    document.querySelector(".temp").innerText  = temp + " ° C";
+    document.querySelector(".humidity").innerText  = "Humidity : " + humidity +"%";
+    document.querySelector(".wind").innerText  = "Wind speed of : "+ speed +" km/h";
+    document.querySelector(".weather").classList.remove("loading");   
+  },
+    search: function(){
+       this.fetchWeather( document.querySelector(".search-bar").value);
+    } 
+};
+document.querySelector(".search button").addEventListener("click", function()
+{
+weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+    if(event.key == 'Enter'){
+        weather.search();
+    } 
+
+});
+weather.fetchWeather("Nairobi");
